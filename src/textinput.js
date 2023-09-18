a261b12f79243579902057fbd3c80534844f8a6c
KylesTech95 <KylesTech95@outlook.com>
2023-09-17 20:19:30 -0400

    update

--- src/textinput.js
+++ src/textinput.js
@@ -8,7 +8,7 @@ export default class TextInput extends React.Component{
         super(props);
         this.state={
             input:"- one- two\n# h1\n## h2\n[This is a link]()\n\n```<h1>Hello World<\h1>```\n\n```\nHello Universe<\h1>\n```\n\n* one\n*\ntwo\n*\nthree\n\n> ONE line block\n\n> TWO line block\n\n![Picture of an apple](https://m.media-amazon.com/images/I/51conNadQtL._SX300_SY300_QL70_FMwebp_.jpg)\n\n**strong text** __strong text__"
-        }got om
+        }
 
     }
     handleChange = (event) => {
